83a3815162c34103ef870ab7daeb57e07234ec07
Dakota Chambers <dchambers@zendesk.com>
2020-07-29 14:24:29 -0500

    Add symlink for notekeeper

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,7 +2,8 @@
 
 # Use this script to automatically create symlinks to `~` for *everything* in this repo.
 
-ln -sv $PWD/.vimrc ~
-ln -sv $PWD/.tmux.conf ~
-ln -sv $PWD/.bash_profile ~
-ln -sv $PWD/.config/ranger ~/.config
+ln -sv $PWD/.vimrc                    ~
+ln -sv $PWD/.tmux.conf                ~
+ln -sv $PWD/.bash_profile             ~
+ln -sv $PWD/.config/ranger            ~/.config
+ln -sv $PWD/.config/notekeeper        ~/.config
