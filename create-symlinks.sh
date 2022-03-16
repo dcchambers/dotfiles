bfc2fc439c60ad1b2ad42ae5791b7533e50b9262
Dakota Chambers <dakotachambers@gmail.com>
2022-03-16 08:36:57 -0500

    Add a public gitconfig file

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -7,6 +7,7 @@ ln -sv $PWD/.tmux.conf                ~
 ln -sv $PWD/.bash_profile             ~
 ln -sv $PWD/.ticker.yaml              ~
 ln -sv $PWD/.cobra.yaml               ~
+ln -sv $PWD/.gitconfig-public         ~
 ln -sv $PWD/.config/ranger            ~/.config
 ln -sv $PWD/.config/notekeeper        ~/.config
 ln -sv $PWD/.gnupg/gpg-agent.conf     ~/.gnupg
