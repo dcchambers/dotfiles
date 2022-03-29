4797d4f8ec065f12a08156101ae187f647e7918e
Dakota Chambers <dakotachambers@gmail.com>
2022-03-29 09:59:54 -0500

    Fix .config and .gnupg symlink creation in create-symlinks.sh

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,12 +2,15 @@
 
 # Use this script to automatically create symlinks to `~` for *everything* in this repo.
 
+mkdir -p ~/.config
+mkdir -p ~/.gnupg
+
 ln -sv $PWD/.vimrc                    ~
 ln -sv $PWD/.tmux.conf                ~
 ln -sv $PWD/.bash_profile             ~
 ln -sv $PWD/.ticker.yaml              ~
 ln -sv $PWD/.cobra.yaml               ~
 ln -sv $PWD/.gitconfig-public         ~
-ln -sv $PWD/.config/ranger            ~/.config
-ln -sv $PWD/.config/notekeeper        ~/.config
-ln -sv $PWD/.gnupg/gpg-agent.conf     ~/.gnupg
+ln -sv $PWD/.config/ranger            ~/.config/ranger
+ln -sv $PWD/.config/notekeeper        ~/.config/notekeeper
+ln -sv $PWD/.gnupg/gpg-agent.conf     ~/.gnupg/gpg-agent.conf
