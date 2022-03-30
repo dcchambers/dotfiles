9dc65b99d95847f33e1095262c7ea778aa86cb2c
Dakota Chambers <dakotachambers@gmail.com>
2022-03-30 12:48:20 -0500

    Add vscode settings.json file

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -4,6 +4,7 @@
 
 mkdir -p ~/.config
 mkdir -p ~/.gnupg
+mkdir -p ~/Library/Application\ Support/Code/User
 
 ln -sv $PWD/.vimrc                    ~
 ln -sv $PWD/.tmux.conf                ~
@@ -14,3 +15,4 @@ ln -sv $PWD/.gitconfig-public         ~
 ln -sv $PWD/.config/ranger            ~/.config/ranger
 ln -sv $PWD/.config/notekeeper        ~/.config/notekeeper
 ln -sv $PWD/.gnupg/gpg-agent.conf     ~/.gnupg/gpg-agent.conf
+ln -sv $PWD/vscode/settings.json      ~/Library/Application\ Support/Code/User/settings.json
