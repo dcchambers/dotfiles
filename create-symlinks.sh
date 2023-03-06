29f8824279bb1e2abdd03e74f1e3c4b16c8e3e98
Dakota Chambers <dakotachambers@gmail.com>
2023-03-06 14:24:21 -0600

    Update symlink script for gh config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,6 +3,7 @@
 # Use this script to automatically create symlinks to `~` for *everything* in this repo.
 
 mkdir -p ~/.config
+mkdir -p ~/.config/gh
 mkdir -p ~/.config/notekeeper
 mkdir -p ~/.gnupg
 mkdir -p ~/Library/Application\ Support/Code/User
@@ -14,6 +15,7 @@ ln -sv $PWD/.bash_profile             ~
 ln -sv $PWD/.ticker.yaml              ~
 ln -sv $PWD/.cobra.yaml               ~
 ln -sv $PWD/.gitconfig-public         ~
+ln -sv $PWD/.config/gh/config.yml     ~/.config/gh/config.yml
 ln -sv $PWD/.config/notekeeper/noterc ~/.config/notekeeper/noterc
 ln -sv $PWD/.gnupg/gpg-agent.conf     ~/.gnupg/gpg-agent.conf
 ln -sv $PWD/vscode/settings.json      ~/Library/Application\ Support/Code/User/settings.json
