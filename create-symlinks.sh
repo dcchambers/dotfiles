81dd01546523c513419fa1dd54873970a85efd17
Dakota Chambers <dakotachambers@gmail.com>
2024-06-04 15:45:02 -0500

    Add alacritty config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,21 +3,23 @@
 # Use this script to automatically create symlinks to `~` for *everything* in this repo.
 
 mkdir -p ~/.config
+mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/gh
 mkdir -p ~/.config/notekeeper
 mkdir -p ~/.gnupg
 mkdir -p ~/Library/Application\ Support/Code/User
 mkdir -p ~/.newsboat
 
-ln -sv $PWD/.vimrc                    ~
-ln -sv $PWD/.tmux.conf                ~
-ln -sv $PWD/.bash_profile             ~
-ln -sv $PWD/.bashrc                   ~
-ln -sv $PWD/.ticker.yaml              ~
-ln -sv $PWD/.cobra.yaml               ~
-ln -sv $PWD/.gitconfig-public         ~
-ln -sv $PWD/.config/gh/config.yml     ~/.config/gh/config.yml
-ln -sv $PWD/.config/notekeeper/noterc ~/.config/notekeeper/noterc
-ln -sv $PWD/.gnupg/gpg-agent.conf     ~/.gnupg/gpg-agent.conf
-ln -sv $PWD/vscode/settings.json      ~/Library/Application\ Support/Code/User/settings.json
-ln -sv $PWD/.newsboat/urls            ~/.newsboat/urls
+ln -sv $PWD/.vimrc                           ~
+ln -sv $PWD/.tmux.conf                       ~
+ln -sv $PWD/.bash_profile                    ~
+ln -sv $PWD/.bashrc                          ~
+ln -sv $PWD/.ticker.yaml                     ~
+ln -sv $PWD/.cobra.yaml                      ~
+ln -sv $PWD/.gitconfig-public                ~
+ln -sv $PWD/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sv $PWD/.config/gh/config.yml            ~/.config/gh/config.yml
+ln -sv $PWD/.config/notekeeper/noterc        ~/.config/notekeeper/noterc
+ln -sv $PWD/.gnupg/gpg-agent.conf            ~/.gnupg/gpg-agent.conf
+ln -sv $PWD/vscode/settings.json             ~/Library/Application\ Support/Code/User/settings.json
+ln -sv $PWD/.newsboat/urls                   ~/.newsboat/urls
