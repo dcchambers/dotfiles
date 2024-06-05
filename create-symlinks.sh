62c3eadfdd8cf68d072c40f044e82a5a0b99e135
Dakota Chambers <dakotachambers@gmail.com>
2024-06-04 23:59:52 -0500

    Add lazyvim config to dotfiles

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -7,6 +7,10 @@ mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/alacritty/themes
 mkdir -p ~/.config/gh
 mkdir -p ~/.config/notekeeper
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/nvim/lua
+mkdir -p ~/.config/nvim/lua/config
+mkdir -p ~/.config/nvim/lua/plugins
 mkdir -p ~/.gnupg
 mkdir -p ~/Library/Application\ Support/Code/User
 mkdir -p ~/.newsboat
@@ -23,6 +27,13 @@ ln -sv $PWD/.config/alacritty/themes/monokai_pro.toml     ~/.config/alacritty/th
 ln -sv $PWD/.config/alacritty/themes/solarized_light.toml ~/.config/alacritty/themes/solarized_light.toml
 ln -sv $PWD/.config/gh/config.yml                         ~/.config/gh/config.yml
 ln -sv $PWD/.config/notekeeper/noterc                     ~/.config/notekeeper/noterc
+ln -sv $PWD/lazyvim/init.lua                      ~/.config/nvim/init.lua
+ln -sv $PWD/lazyvim/lua/config/autocmds.lua       ~/.config/nvim/lua/config/autocmds.lua
+ln -sv $PWD/lazyvim/lua/config/keymaps.lua        ~/.config/nvim/lua/config/keymaps.lua
+ln -sv $PWD/lazyvim/lua/config/lazy.lua           ~/.config/nvim/lua/config/lazy.lua
+ln -sv $PWD/lazyvim/lua/config/options.lua        ~/.config/nvim/lua/config/options.lua
+ln -sv $PWD/lazyvim/lua/plugins/example.lua       ~/.config/nvim/lua/plugins/example.lua
+ln -sv $PWD/lazyvim/stylua.toml                   ~/.config/nvim/stylua.toml
 ln -sv $PWD/.gnupg/gpg-agent.conf                         ~/.gnupg/gpg-agent.conf
 ln -sv $PWD/vscode/settings.json                          ~/Library/Application\ Support/Code/User/settings.json
 ln -sv $PWD/.newsboat/urls                                ~/.newsboat/urls
